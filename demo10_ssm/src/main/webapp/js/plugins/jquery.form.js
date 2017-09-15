53ec6b1b8d12062c68196a38ad8c534c875c4c24
Mole Developer <moledeveloper@Moles-iMac.local>
2017-09-15 16:40:48 +0800

    1

--- demo10_ssm/src/main/webapp/js/plugins/jquery.form.js
+++ demo10_ssm/src/main/webapp/js/plugins/jquery.form.js
@@ -25,7 +25,7 @@
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
+		define(['../jquery/jquery'], factory);
 	} else if (typeof module === 'object' && module.exports) {
 		// Node/CommonJS
 		module.exports = function( root, jQuery ) {
@@ -33,10 +33,10 @@
 				// require('jQuery') returns a factory that requires window to build a jQuery instance, we normalize how we use modules
 				// that require this pattern but the window provided is a noop if it's defined (how jquery works)
 				if (typeof window !== 'undefined') {
-					jQuery = require('jquery');
+					jQuery = require('../jquery/jquery');
 				}
 				else {
-					jQuery = require('jquery')(root);
+					jQuery = require('../jquery/jquery')(root);
 				}
 			}
 			factory(jQuery);
