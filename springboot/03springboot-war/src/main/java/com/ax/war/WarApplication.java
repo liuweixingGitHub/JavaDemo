2a4b458b50983310e893da6888a015b0c57ad235
axinger <>
2020-01-06 22:42:10 +0800

    update

--- springboot/03springboot-war/src/main/java/com/ax/war/WarApplication.java
+++ springboot/03springboot-war/src/main/java/com/ax/war/WarApplication.java
@@ -2,11 +2,18 @@ package com.ax.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WarApplication {
+public class WarApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WarApplication.class);
+	}
+
+		public static void main(String[] args) {
 		SpringApplication.run(WarApplication.class, args);
 	}
 
