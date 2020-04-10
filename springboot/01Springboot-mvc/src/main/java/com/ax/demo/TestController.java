58d0326d3d0ee052b588c92c83c620b9cfd2f564
xingHome <>
2020-04-10 21:49:22 +0800

    update…

---
+++ springboot/01Springboot-mvc/src/main/java/com/ax/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.ax.demo;
+
+import com.ax.demo.entity.Student;
+
+public class TestController {
+
+    public void init(){
+
+
+        Student student = new Student(1,"jim");
+
+
+        Student.builder().id(1).name("jim");
+
+
+    }
+}
---
+++ springboot/01Springboot-mvc/src/main/java/com/ax/demo/entity/Student.java
@@ -0,0 +1,23 @@
+package com.ax.demo.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor                 //无参构造
+@AllArgsConstructor                //有参构造
+@ToString
+@Builder
+public class Student implements Serializable {
+
+
+    ///忽略set方法
+    @Setter(AccessLevel.NONE)
+    private int id;
+    private String name;
+
+
+
+}
