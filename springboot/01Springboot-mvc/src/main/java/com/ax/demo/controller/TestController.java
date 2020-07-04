3eed8b74d434e681fcb01c673be923d90fc04134
axinger <liu_weixing@qq.com>
2020-07-04 19:21:49 +0800

    update…

--- springboot/01Springboot-mvc/src/main/java/com/ax/demo/controller/TestController.java
+++ springboot/01Springboot-mvc/src/main/java/com/ax/demo/controller/TestController.java
@@ -1,17 +1,70 @@
 package com.ax.demo.controller;
 
+import com.ax.demo.dto.StudentDto;
 import com.ax.demo.entity.Student;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
 public class TestController {
 
-    public void init(){
+    @PostMapping(value = "/test0", produces = "application/json;charset=UTF-8")
+    public Object test(@RequestBody StudentDto studentDto) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("date", studentDto);
+
+        return map;
+    }
+
+    /**
+     * @RequestBody能把简单json结构参数转换成实体类，
+     */
+    @RequestMapping(value = "/test1")
+    public Map<String, Object> test1(@RequestBody @Validated StudentDto studentDto) {
+        System.out.println("studentDto = " + studentDto);
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("date", studentDto);
+
+        return map;
+    }
+
+    @RequestMapping(value = "/test2")
+    public Object test2(StudentDto studentDto) {
+        System.out.println("studentDto2 = " + studentDto);
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("date", studentDto);
+
+        return map;
+    }
+
+    public void init() {
 
 
-        Student student = new Student(1,"jim");
+        Student student = new Student(1, "jim");
 
 
         Student.builder().id(1).name("jim");
 
 
     }
+
+
+    @RequestMapping(value = "/list")
+    public void listParam(@RequestBody List<String> list, String name) {
+
+        System.out.println("list = " + list);
+        System.out.println("name = " + name);
+    }
+
 }
--- springboot/01Springboot-mvc/src/main/java/com/ax/demo/controller/UIController.java
+++ springboot/01Springboot-mvc/src/main/java/com/ax/demo/controller/UIController.java
@@ -3,12 +3,11 @@ package com.ax.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-public class OtherController {
+public class UIController {
 
     @RequestMapping(value = "/api")
-    public String index() {
+    public String api() {
         System.out.println("swagger-ui.html");
         return "redirect:swagger-ui.html";
     }
